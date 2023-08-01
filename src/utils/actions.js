addba7ca79eee876acd268bfde80dc6f7bd6cb05
KonradSosna <konrad.sosna.95@gmail.com>
2023-08-01 16:44:50 +0200

    init

---
+++ src/utils/actions.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const getUserList = async (page) => {
+	try {
+		const { data } = await axios.get(
+			`https://reqres.in/api/users?per_page=2&page=${page}`
+		);
+		return data;
+	} catch (error) {
+		return window.alert(error.message);
+	}
+};
